1c0897d42fcdcabce43cb3f16a1c7eb24332c334
sachin dhakad <103819602+TheSachinDhakad@users.noreply.github.com>
2022-09-04 17:05:36 +0530

    Update views.py

--- django_tutorial/textutils/textutils/views.py
+++ django_tutorial/textutils/textutils/views.py
@@ -69,64 +69,6 @@ def analyze(request):
 
 
 
-# # i have created this file
-# from django.http import HttpResponse
-# from django.shortcuts import render
-# def index(request):
-#     # return HttpResponse('''<h1>sachin</h1> <a href="https://www.google.com/" target="_blank" >go to google</a>''')
-#
-#     return render(request,'index.html')
-# def About(request):
-#     return HttpResponse("this is about page")
-# def Contact(request):
-#     return HttpResponse("this is contact page")
-# def analyze(request):
-#     # return HttpResponse("this is analyze page")
-#     djtext = request.POST.get('text' , 'default')
-#     # print(djtext)
-#     removepuc = request.POST.get('removepuc' , 'off')
-#     fullcaps = request.POST.get('fullcaps' , 'off')
-#     newlineremover = request.POST.get('newlineremover' , 'off')
-#     extraspaceremover = request.POST.get('extraspaceremover' , 'off')
-#     # print(removepuc)
-#
-#
-#     if removepuc=='on':
-#         punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
-#         analyzed = ""
-#         for char in djtext:
-#             if char not in punctuations:
-#                 analyzed = analyzed + char
-#         # analyzed = djtext
-#         params = {'purpose':'Remove punchtuation' , 'analyzed_text':analyzed}
-#         djtext = analyzed
-#         # return render(request,'analyze.html',params)
-#     if fullcaps=='on':
-#         analyzed = ""
-#         for char in djtext:
-#             analyzed = analyzed+ char.upper()
-#         params = {'purpose': 'Change to upper case', 'analyzed_text': analyzed}
-#         djtext = analyzed
-#         # return render(request, 'analyze.html', params)
-#
-#     if extraspaceremover == "on":
-#         analyzed = ""
-#         for index , char in enumerate(djtext):
-#             if not(djtext[index]=="  " and djtext[index+1]=="  "):
-#                 analyzed = analyzed+char
-#             params = {'purpose': 'extra space remove', 'analyzed_text': analyzed}
-#             djtext=analyzed
-#             # return render(request, 'analyze.html', params)
-#     if newlineremover=="on":
-#         analyzed = ""
-#         for char in djtext:
-#             if char != "\n" and char!="\r":
-#                 analyzed = analyzed +char
-#         params = {'purpose': 'extra space remove', 'analyzed_text': analyzed}
-#         djtext = analyzed
-#         # return render(request, 'analyze.html', params)
-#     # else:
-#     #     return HttpResponse("error")
-#     return render(request, 'analyze.html', params)
+
 
 
